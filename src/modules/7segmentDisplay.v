0b4f6e2fa68bf9cbdb2c9ab56ddb5fd87664b2ac
Matheus Serrao Uchoa <maktheus@gmail.com>
2023-05-17 12:50:50 +0000

    update seven segments

--- src/modules/7segmentDisplay.v
+++ src/modules/7segmentDisplay.v
@@ -1,3 +1,9 @@
-module sevenSegmentes;
+module sevenSegmentes(
+                    input wire [7:0] binario,
+                    input wire clk,
+                    output reg [7:0] seven_segments_signal_display
+                    );
+
+                    
 
 endmodule
